9484cee47adb9be5b55ad2ed4e173416868f7549
Roman Jackson <romanj@uchicago.edu>
2026-07-03 01:49:12 -0500

    added max_corner precomps

--- precomputations.h
+++ precomputations.h
@@ -8,6 +8,7 @@
 #include <utility>
 #include <algorithm>
 
+// pre-condition: each lookup is canonicalized
 inline constexpr auto ORIENTATIONS_PRECOMPUTED = []() {
     util::fast_map<bitmap, util::dynamic_array<bitmap, MAXIMUM_ORIENTATIONS>, NUM_PIECES> m{};
     auto it { m.data.begin() };
@@ -20,14 +21,15 @@ inline constexpr auto ORIENTATIONS_PRECOMPUTED = []() {
     return m;
 }();
 
+// pre-condition: each lookup is normalized
 inline constexpr auto TO_CANONICAL_PRECOMPUTED = []() {
     util::fast_map<bitmap, bitmap, MAXIMUM_ORIENTATIONS * NUM_PIECES> m{};
     auto it { m.data.begin() };
     for (auto piece : std::views::keys(AVAILABLE_PIECES)) {
         piece.canonicalize();
         for (auto orient : generate_all_orientations(piece)) {
-            it->first = piece;
-            it->second = orient;
+            it->first = orient;
+            it->second = piece;
             it++;
         }
     }
@@ -41,19 +43,34 @@ inline constexpr auto MAX_PIECE_BITS = []() {
 }();
 
 inline constexpr auto VALID_BITS_PRECOMPUTED = []() {
-    util::fast_map<bitmap, util::dynamic_array<std::pair<size_t, size_t>, MAX_PIECE_BITS>, NUM_PIECES> m{};
+    util::fast_map<bitmap, util::dynamic_array<std::pair<size_t, size_t>, MAX_PIECE_BITS>, NUM_PIECES * MAXIMUM_ORIENTATIONS> m{};
     auto it { m.data.begin() };
-    for (auto piece : std::views::keys(AVAILABLE_PIECES)) {
-        piece.canonicalize();
-        it->first = piece;
-        for (auto row : std::views::iota(0uz, GRID_SIZE)) {
-            for (auto col : std::views::iota(0uz, GRID_SIZE)) {
-                if (piece.test(col, row)) {
-                    it->second.push_back(std::pair(col, row));
+    for (const auto& piece : std::views::keys(AVAILABLE_PIECES)) {
+        for (const auto& orient : generate_all_orientations(piece)) {
+            it->first = orient;
+            for (auto row : std::views::iota(0uz, GRID_SIZE)) {
+                for (auto col : std::views::iota(0uz, GRID_SIZE)) {
+                    if (orient.test(col, row)) {
+                        it->second.push_back(std::pair(col, row));
+                    }
                 }
             }
-        }
         it++;
+        }
+    }
+    return m;
+}();
+
+// pre-condition: each lookup is normalized
+inline constexpr auto MAX_CORNER_PRECOMPUTED = []() {
+    util::fast_map<bitmap, std::pair<size_t, size_t>, NUM_PIECES * MAXIMUM_ORIENTATIONS> m{};
+    auto it { m.data.begin() };
+    for (const auto& piece : std::views::keys(AVAILABLE_PIECES)) {
+        for (const auto& orient : generate_all_orientations(piece)) {
+            it->first = orient;
+            it->second = orient.max_corner();
+            it++;
+        }
     }
     return m;
 }();
